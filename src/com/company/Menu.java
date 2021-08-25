6861e48f70459a1f130bb78c94c78fb9ae592120
Simon Morris <simonjmorris213@gmail.com>
2021-08-24 19:17:55 -0700

    car

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -17,42 +17,48 @@ public class Menu {
 
     public void run() {
 
-        System.out.println("\n---\nWe have a total of " + arrList.size() + " cars available and " + rented.size() + " cars being rented.");
-        System.out.println("\nChoose from the following menus: ");
-        System.out.println("1. Rent a car");
-        System.out.println("2. Return a car");
+        while (arrList.size() != 0) {
 
-        int input = userInput.getInt(1, 2);
-
-        if (input == 1) {
-
-        }
+            System.out.println("\nThis is our current selection of rental cars:");
+            for (int i = 0; i < arrList.size(); i++) {
+                System.out.println(i + 1 + ") " + arrList.get(i));
+            }
 
             System.out.println("Please choose the car you wish to rent!");
             int selection = userInput.getInt(1, arrList.size());
 
             for (int i = 0; i < arrList.size(); i++) {
                 if (selection - 1 == i) {
-                    rentedTotal(i);
+                    //System.out.println("\n---\nThank you for choosing the " + arrList.get(i) + "! Safe Travels!\n---");
+                    rentedTotal(i); //For asking customer how long they wish to rent the car for and what the total cost will be.
                     rented.add(arrList.get(i));
                     arrList.remove(i);
-                    run();
+                    subMenu();
                 }
-
             }
-
         }
+    }
 
     private void subMenu() {
-        System.out.println("\nThank you for choosing The Car Rental Company! What would you like to do from this point?");
+        System.out.println("\nThank you for choosing the Car Rental Company! What would you like to do from this point?");
         System.out.println("1) Restart this program and refresh the lot");
-        System.out.println("2) Exit the program");
-        int selection = userInput.getInt(1, 2);
+        System.out.println("2) Return to the menu for another rental.");
+        System.out.println("3) Exit the program");
+        int selection = userInput.getInt(1, 3);
 
         if (selection == 1) {
             System.out.println("\n---\nRestarting the program!\n---");
-            start();
-        } else {
+            if (arrList.size() != 0){
+                System.out.println("\n---\nThe list still has cars remaining! Please empty the list before restarting.\n---\n");
+                run();
+            }
+            else {
+                start();
+            }
+        } else if(selection == 2){
+            System.out.println("\n---\nReturning to the main menu.\n---");
+        }
+        else {
             new CLI().exit();
         }
     }
@@ -62,7 +68,7 @@ public class Menu {
         System.out.println("\n---\nThank you for choosing the " + arrList.get(i) + "! The price per day is " + arrList.get(i).getPrice() + ". How many days do you wish to rent it?\n---");
         int input = userInput.getInt();
         int total = input * arrList.get(i).getPrice();
-        System.out.println("\n---\n For a " + input + " day rental the " + arrList.get(i) + " will cost a total of "  + total + "! Safe Travels!\n---");
+        System.out.println("\n---\n For a " + input + " day rental the " + arrList.get(i) + " will cost a total of "  + total + "!\n---");
 
     }
 
